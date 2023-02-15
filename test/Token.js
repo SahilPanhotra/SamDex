113d91b5ed2d7ae9d2ac2017606867bed0804e25
SahilPanhotra <sahilpanhotra@gmail.com>
2023-02-15 16:38:50 +0530

    Add Contract tests

---
+++ test/Token.js
@@ -0,0 +1,37 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+const tokens = (n) => {
+  return ethers.utils.parseUnits(n.toString(), "ether");
+};
+
+describe("Token", () => {
+  let token;
+  beforeEach(async () => {
+    const Token = await ethers.getContractFactory("Token");
+    token = await Token.deploy("MY TOKEN", "SAM", 1000000);
+  });
+
+  describe("Deployment", () => {
+    const name = "MY TOKEN";
+    const symbol = "SAM";
+    const decimals = 18;
+    const totalSupply = tokens(1000000);
+
+    it("has Correct name", async () => {
+      expect(await token.name()).to.equal(name);
+    });
+
+    it("has Correct Symbol", async () => {
+      expect(await token.symbol()).to.equal(symbol);
+    });
+
+    it("has Correct decimals", async () => {
+      expect(await token.decimals()).to.equal(decimals);
+    });
+
+    it("has correct  total Supply", async () => {
+      expect(await token.totalSupply()).to.equal(totalSupply);
+    });
+  });
+});
