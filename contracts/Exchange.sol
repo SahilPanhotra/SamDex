2858a014f069aefa2c37d89300ccd5879e310536
SahilPanhotra <sahilpanhotra@gmail.com>
2023-02-21 19:40:29 +0530

    making orders function

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -8,10 +8,35 @@ contract Exchange {
     uint256 public feePercent;
 
     mapping(address => mapping(address => uint256)) public tokens;
+    mapping(uint256 => _Order) public orders;
+    uint256 public ordersCount;
 
     event Deposit(address token, address user, uint256 amount, uint256 balance);
-    event Withdraw(address token, address user, uint256 amount, uint256 balance);
+    event Withdraw(
+        address token,
+        address user,
+        uint256 amount,
+        uint256 balance
+    );
+    event Order(
+        uint256 id,
+        address user,
+        address tokenGet,
+        uint256 amountGet,
+        address tokenGive,
+        uint256 amountGive,
+        uint256 timestamp
+    );
 
+    struct _Order {
+        uint256 id;
+        address user;
+        address tokenGet;
+        uint256 amountGet;
+        address tokenGive;
+        uint256 amountGive;
+        uint256 timestamp;
+    }
 
     constructor(address _feeAccount, uint256 _feePercent) {
         feeAccount = _feeAccount;
@@ -25,12 +50,11 @@ contract Exchange {
     }
 
     function withdrawToken(address _token, uint256 _amount) public {
-        require(tokens[_token][msg.sender]>=_amount);
+        require(tokens[_token][msg.sender] >= _amount);
         Token(_token).transfer(msg.sender, _amount);
         tokens[_token][msg.sender] = tokens[_token][msg.sender] - _amount;
         emit Withdraw(_token, msg.sender, _amount, tokens[_token][msg.sender]);
     }
-
 
     function balanceOf(address _token, address _user)
         public
@@ -38,5 +62,37 @@ contract Exchange {
         returns (uint256)
     {
         return tokens[_token][_user];
+    }
+
+    function makeOrder(
+        address _tokenGet,
+        uint256 _amountGet,
+        address _tokenGive,
+        uint256 _amountGive
+    ) public {
+
+        require(balanceOf(_tokenGive, msg.sender) >= _amountGive);
+        
+        ordersCount = ordersCount + 1;
+
+        orders[ordersCount] = _Order(
+            ordersCount,
+            msg.sender,
+            _tokenGet,
+            _amountGet,
+            _tokenGive,
+            _amountGive,
+            block.timestamp
+        );
+
+        emit Order(
+            ordersCount,
+            msg.sender,
+            _tokenGet,
+            _amountGet,
+            _tokenGive,
+            _amountGive,
+            block.timestamp
+        );
     }
 }
