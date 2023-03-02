fe87dfa99d53245705f40afd888804f3432f57b0
SahilPanhotra <sahilpanhotra@gmail.com>
2023-03-02 21:46:06 +0530

    feat:withdraw tokens

--- src/store/interactions.js
+++ src/store/interactions.js
@@ -77,6 +77,9 @@ export const subscribeToEvents = (exchange, dispatch) => {
   exchange.on("Deposit", (token, user, amount, balance, event) => {
     dispatch(TRANSFER_SUCCESS({ event }));
   });
+  exchange.on("Withdraw", (token, user, amount, balance, event) => {
+    dispatch(TRANSFER_SUCCESS({ event }));
+  });
 };
 
 // ------------------------------------------------------------------------------
@@ -123,15 +126,19 @@ export const transferTokens = async (
   try {
     const signer = await provider.getSigner();
     const amountToTransfer = ethers.utils.parseUnits(amount.toString(), 18);
-
-    transaction = await token
-      .connect(signer)
-      .approve(exchange.address, amountToTransfer);
-    await transaction.wait();
-    transaction = await exchange
-      .connect(signer)
-      .depositToken(token.address, amountToTransfer);
-
+    if (transferType === "Deposit") {
+      transaction = await token
+        .connect(signer)
+        .approve(exchange.address, amountToTransfer);
+      await transaction.wait();
+      transaction = await exchange
+        .connect(signer)
+        .depositToken(token.address, amountToTransfer);
+    } else {
+      transaction = await exchange
+        .connect(signer)
+        .withdrawToken(token.address, amountToTransfer);
+    }
     await transaction.wait();
   } catch (error) {
     dispatch(TRANSFER_FAIL());
