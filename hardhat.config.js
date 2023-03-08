63acd65895683e683387405dbb7b3316fd8d7a6f
SahilPanhotra <sahilpanhotra@gmail.com>
2023-03-08 22:23:26 +0530

    deploy:make config deployable to mumbai network

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,9 +1,18 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config({ path: ".env" });
+
+const { ALCHEMY_API_KEY_URL } = process.env;
+
+const { MUMBAI_PRIVATE_KEY } = process.env;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.18",
   networks: {
     hardhat: {},
+    mumbai: {
+      url: ALCHEMY_API_KEY_URL,
+      accounts: [MUMBAI_PRIVATE_KEY],
+    },
   },
 };
